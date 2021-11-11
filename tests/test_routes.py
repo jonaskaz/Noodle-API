accb116fe34d4ea5666b821282167b6a60e279f4
jonas <jonas.kazlauskas@me.com>
2021-11-11 13:00:20 -0500

    Update tests

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -3,20 +3,17 @@
 def test_post_order(client, order_payload):
     res = client.post("/order", json = order_payload)
     assert res.status_code == 200
-    assert res.json() == {'mode': 0, 'flavor': 'beef', 'toppings': ['onions', 'spice']}
 
 
 def test_post_game(client, game_payload):
     res = client.post("/order", json = game_payload)
     assert res.status_code == 200
-    data = res.json()
-    assert data["mode"] == 1
-    assert data["flavor"] == "beef"
 
 
 def test_get_order(client):
     res1 = client.get("/order")
     assert res1.json()["mode"] == 0
+    assert res1.json()["toppings"] == ["onions", "spice"]
     res2 = client.get("/order")
     assert res2.json()["mode"] == 1
     res3 = client.get("/order")
